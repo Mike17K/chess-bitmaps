2f3cfb60001f2dbaa4cf9412cf165b23a1ab85ee
unknown <kaipismike1@gmail.com>
2023-03-04 20:10:12 +0200

    added rook moves also impruved the structure

--- functions/board.h
+++ functions/board.h
@@ -10,40 +10,37 @@ using namespace std;
 class Board
 {
 public:
+    bool isWhiteToPlay;
+    bool castlingK;
+    bool castlingk;
+    bool castlingQ;
+    bool castlingq;
+
     uint64_t bK;
     uint64_t wK;
-
     uint64_t bQ;
     uint64_t wQ;
-
     uint64_t bB;
     uint64_t wB;
-
     uint64_t bR;
     uint64_t wR;
-
     uint64_t bp;
     uint64_t wp;
-
     uint64_t bN;
     uint64_t wN;
+
+    uint64_t bA;
+    uint64_t wA;
 
     Board();
     Board(char state[]);
 
-    uint64_t rookAttacks_w();
-    uint64_t horseAttacks_w();
-    uint64_t qeenAttacks_w();
-    uint64_t kingAttacks_w();
-    uint64_t pownAttacks_w();
-    uint64_t bishopAttacks_w();
-
-    uint64_t rookAttacks_b();
-    uint64_t horseAttacks_b();
-    uint64_t qeenAttacks_b();
-    uint64_t kingAttacks_b();
-    uint64_t pownAttacks_b();
-    uint64_t bishopAttacks_b();
+    void rookAttacksGen();
+    void nightAttacksGen();
+    void qeenAttacksGen();
+    void kingAttacksGen();
+    void pownAttacksGen();
+    void bishopAttacksGen();
 
     void setBoard(char state[]);
 
--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+void timefunction(void *func);
+
 int main()
 {
   /* Gives representation of
@@ -21,32 +23,42 @@ int main()
       0  1  2  3  4  5  6  7
   */
 
-  Board *b = new Board("r1bqkbnr/ppp1pppp/2n5/3p4/3P4/5N2/PPP1PPPP/RNBQKB1R w KQkq d6 3 2");
+  Board *b = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
-  bShow(b->bB);
+  bShow(b->wK | b->bK | b->wQ | b->bQ | b->wN | b->bN | b->wB | b->bB | b->wR | b->bR | b->bp | b->wp);
 
-  /*
-    int i = 0;
-    std::chrono::seconds duration(1); // 1 second duration
-    // loop for 1 second
-    auto start = std::chrono::high_resolution_clock::now();
-    while (std::chrono::high_resolution_clock::now() - start < duration) {
-      i++;
-    }
+  cout << sizeof(*b) << endl;
 
-    std::cout << "i increased to " << i << std::endl;
+  b->rookAttacksGen();
+  b->nightAttacksGen();
 
+  bShow(b->wA);
+  bShow(b->bA);
 
-    i = 0;
-    // loop for 1 second
-    start = std::chrono::high_resolution_clock::now();
-    while (std::chrono::high_resolution_clock::now() - start < duration) {
-      i++;
-      // function to time
+  //*
+  int i = 0;
+  std::chrono::milliseconds duration(100); // 1 second duration
+  // loop for 1 second
+  auto start = std::chrono::high_resolution_clock::now();
+  while (std::chrono::high_resolution_clock::now() - start < duration)
+  {
+    i++;
+  }
 
-    }
+  std::cout << "i increased to " << i << std::endl;
 
-    std::cout << "i increased to " << i << std::endl;
+  int i2 = 0;
+  // loop for 1 second
+  start = std::chrono::high_resolution_clock::now();
+  while (std::chrono::high_resolution_clock::now() - start < duration)
+  {
+    i2++;
+    b->rookAttacksGen();
+    b->nightAttacksGen();
+    //    function to time
+  }
+
+  std::cout << "i2 increased to " << i2 << std::endl;
+  std::cout << "Function score is " << (float)(i2) / i * 100 << "% of the max." << std::endl;
   //*/
-  return 0;
 }
